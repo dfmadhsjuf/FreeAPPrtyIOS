6a432eca4c65a95666165630a6d900f142797eab
alumno <alumno@Mac-mini-de-administrador-3.local>
2018-01-24 10:37:41 +0100

    Initial Commit

---
+++ FreeAPPrty/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FreeAPPrty
+//
+//  Created by alumno on 24/1/18.
+//  Copyright © 2018 alumno. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FreeAPPrty/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FreeAPPrty
+//
+//  Created by alumno on 24/1/18.
+//  Copyright © 2018 alumno. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
